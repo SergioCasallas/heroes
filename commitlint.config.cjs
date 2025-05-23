d5502a0b47834cf4e5667e6c16de78833b286389
sergiocasallas <sergiocasallas01@gmail.com>
2025-05-22 20:22:58 -0500

    Esto es un commit de prueba

---
+++ commitlint.config.cjs
@@ -0,0 +1,9 @@
+    // commitlint.config.cjs
+    module.exports = {
+        extends: ['@commitlint/config-conventional'],
+        // Puedes añadir reglas personalizadas aquí si lo deseas
+        // rules: {
+        //   'header-max-length': [2, 'always', 100], // Ejemplo: Limitar la longitud del encabezado a 100 caracteres
+        // }
+      };
+      
